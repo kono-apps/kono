8bcd1118aa21c26de38557aa5c6baff2e175715f
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-07-01 10:58:05 +0300

    Add some docs

--- runtime/src/main/kotlin/kono/webview/asset.kt
+++ runtime/src/main/kotlin/kono/webview/asset.kt
@@ -6,14 +6,22 @@ import kono.runtime.nativeRuntime
  * Represents an asset. This includes anything that is passed to the webview,
  * such as HTML, CSS, JS, and any other files.
  *
- * Note: This maintains a pointer to an underlying asset.
+ * Note: This maintains a pointer to an underlying asset. If this
+ * object is garbage collected, the pointer remains uncleaned. We should
+ * consider using Java's new Cleaner utility to avoid memory leaks
  */
 class Asset(
     private val mimeType: String,
     getContent: () -> ByteArray,
 ) {
 
-    private val content by lazy { getContent() }
+    private val content by lazy {
+        getContent()
+    }
 
-    val assetPtr by lazy { nativeRuntime { createAsset(mimeType, content, content.size) } }
+    val assetPtr by lazy {
+        nativeRuntime {
+            createAsset(mimeType, content, content.size)
+        }
+    }
 }
