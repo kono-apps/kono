8ee8fcfce1dc4432f83d225a963f4bf6960a72c5
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-07-07 19:12:18 +0300

    read string directly

--- app/src/main/kotlin/kono/ipc/IpcHandler.kt
+++ app/src/main/kotlin/kono/ipc/IpcHandler.kt
@@ -1,9 +1,7 @@
 package kono.ipc
 
-import com.squareup.moshi.JsonReader
 import kono.app.KonoApplication
 import kono.json.adapterOf
-import okio.Buffer
 
 class IpcHandler(private val app: KonoApplication) {
 
@@ -11,9 +9,7 @@ class IpcHandler(private val app: KonoApplication) {
         request: String,
         context: FunctionContext,
     ) {
-        val reader = JsonReader.of(Buffer().writeUtf8(request))
-
-        when (val requestType = app.moshi.adapterOf<IPCRequest>().fromJson(reader)!!) {
+        when (val requestType = app.moshi.adapterOf<IPCRequest>().fromJson(request)!!) {
             is RunFunctionRequest -> {
                 app.functions.call(requestType, context) { context.webView!!.eval(it) }
             }
