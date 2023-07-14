e42336df4a963344ea3bd1b16474e2c935f3c296
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-07-14 22:55:16 +0300

    document ConfigParser.kt

--- app/src/main/kotlin/kono/json/ConfigParser.kt
+++ app/src/main/kotlin/kono/json/ConfigParser.kt
@@ -5,12 +5,20 @@ import com.squareup.moshi.Moshi
 import kono.app.KonoConfig
 import okio.Buffer
 
+/**
+ * The path to the config file
+ */
+private const val CONFIG = "kono.json"
+
+/**
+ * Parses the application's [KonoConfig] from
+ */
 fun parseConfig(moshi: Moshi): KonoConfig {
-    val file = KonoConfig::class.java.getResourceAsStream("/kono.json")
-        ?: error("Cannot find the embedded kono.json in resources!")
-    val buffer = Buffer()
-    file.use { buffer.readFrom(it) }
     try {
+        val file = KonoConfig::class.java.getResourceAsStream("/$CONFIG")
+            ?: error("Cannot find the embedded kono.json in resources!")
+        val buffer = Buffer()
+        file.use { buffer.readFrom(it) }
         return moshi.adapterOf<KonoConfig>().fromJson(buffer) ?: error("Failed to parse kono.json")
     } catch (e: JsonDataException) {
         throw IllegalStateException("Failed to parse kono.json: " + e.message)
